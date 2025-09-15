777775fca9102c2c93c2f17c2e8d4cab9bce7257
FelipeSouza14 <felipe.dev.2003@gmail.com>
2025-09-15 13:06:43 -0300

    instalando bibliotecas + adicionando ValidationPipes

--- backend/src/tasks/dto/create-task.dto.ts
+++ backend/src/tasks/dto/create-task.dto.ts
@@ -4,7 +4,19 @@ DTO > Data Transfer Object (Objeto de transferencia de dados)
 > Se usa para representar quais dados e em que formatos uma determinada camada aceita e trabalha
 */
 
+import { IsNotEmpty, IsString, MinLength } from "class-validator";
+
 export class CreateTaskDto {
+
+  // Decorators que fazem a validação do tipo de dado. Por exemplo "@IsString" verifica se o dado é uma string.
+  // Podem ser passados mensagens através deles, por exemplo @MinLength({message: "O nome precisa ter mais de 5 caracteres!"})
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @MinLength(5)
+  @IsNotEmpty()
   readonly description: string;
 }
